254f47579ca8f24803813692726b518bfffa6140
Santiago <smbust38@yahoo.com>
2021-07-10 22:32:40 -0300

    ejercicio clase15/modulo4

---
+++ Modulo4/Clase15/routes/contacto.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('contacto');
+});
+
+module.exports = router;
+
+var nodemailer = require("nodemailer");
+
+router.post('/', async(req, res, next) => {
+
+  var nombre = req.body.nombre;
+    var email = req.body.email;
+    var tel = req.body.tel;
+    var mensaje = req.body.mensaje;
+
+    var obj = {
+      to: 'smbust38@yahoo.com',
+      subject: 'CONTACTO WEB',
+      html: nombre + " se contactó a través de la web y quiere más información a este correo : " + email + ". <br> Además, hizo este comentario : " + mensaje + ". <br> Su tel es: " + tel
+        }
+
+    var transport = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
+
+    var info = await transport.sendMail(obj);
+
+    res.render('contacto', {
+      message: 'Mensaje enviado correctamente'
+    });
+});
